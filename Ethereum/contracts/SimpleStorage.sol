03ba2fc3122517767d8366ed1f9e22ebdc901cb1
akash bhardwaj <akkiworkspace@gmail.com>
2021-07-27 12:13:56 +0530

    Deployed to ETH-Testnet (Ropsten)

---
+++ Ethereum/contracts/SimpleStorage.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+    uint public data;
+
+    function setData(uint _data) external {
+        data = _data;
+    }
+}
